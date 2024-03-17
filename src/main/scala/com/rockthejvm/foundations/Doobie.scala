6391ab2968da0de0e73877add67f370990a646f5
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-03-17 21:26:22 +0200

    -typelevelPro: Doobie foundations

--- src/main/scala/com/rockthejvm/foundations/Doobie.scala
+++ src/main/scala/com/rockthejvm/foundations/Doobie.scala
@@ -1,6 +1,9 @@
 package com.rockthejvm.foundations
 
+import cats.effect.kernel.MonadCancelThrow
 import cats.effect.{IO, IOApp}
+import doobie.hikari.HikariTransactor
+import doobie.util.ExecutionContexts
 import doobie.util.transactor.Transactor
 object Doobie extends IOApp.Simple {
 
@@ -43,8 +46,49 @@ object Doobie extends IOApp.Simple {
     action.transact(xa)
   }
 
-  override def run: cats.effect.IO[Unit] = findStudentsByInitial("m").flatMap { students =>
-    IO(println(students))
+  // organize code
+  // define a trait that would act as the repository in the form of a type class
+  trait Students[F[_]] {
+    def findById(id: Int): F[Option[Student]]
+    def findAll: F[List[Student]]
+    def create(name: String): F[Int]
   }
+
+  object Students {
+    import doobie.implicits._
+    def make[F[_]: MonadCancelThrow](xa: Transactor[F]): Students[F] = new Students[F] {
+      def findById(id: Int): F[Option[Student]] =
+        sql"select id, name from students where id = $id".query[Student].option.transact(xa)
+
+      def findAll: F[List[Student]] =
+        sql"select id, name from students".query[Student].to[List].transact(xa)
+
+      def create(name: String): F[Int] =
+        sql"insert into students (name) values ($name)".update
+          .withUniqueGeneratedKeys[Int]("id")
+          .transact(xa)
+    }
+  }
+
+  val postgresResource = for {
+    ce <- ExecutionContexts.fixedThreadPool[IO](16)
+    xa <- HikariTransactor.newHikariTransactor[IO](
+      "org.postgresql.Driver",
+      "jdbc:postgresql://localhost:5432/demo",
+      "docker",
+      "docker",
+      ce
+    )
+  } yield xa
+
+  val smallProgram: IO[Unit] = postgresResource.use { xa =>
+    val studentsRepo = Students.make[IO](xa)
+    for {
+      id   <- studentsRepo.create("Jose")
+      jose <- studentsRepo.findById(id)
+      _    <- IO(println(s"The first student of RockTheJVM is $jose"))
+    } yield ()
+  }
+  override def run: cats.effect.IO[Unit] = smallProgram
 
 }
