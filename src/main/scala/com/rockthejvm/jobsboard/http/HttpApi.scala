9b6eaed9f4c267f0b27d79ad5d4085706a66e69c
Kurohige <jhidalgopalacios@yahoo.com>
2024-03-26 22:27:18 +0200

    added endpoint that units all routes

---
+++ src/main/scala/com/rockthejvm/jobsboard/http/HttpApi.scala
@@ -0,0 +1,24 @@
+package com.rockthejvm.jobsboard.http
+
+import org.http4s.*
+import org.http4s.dsl.*
+import org.http4s.dsl.impl.*
+import org.http4s.server.*
+import cats.Monad
+import com.comcast.ip4s.Literals.idn
+import cats.implicits.*
+
+import com.rockthejvm.jobsboard.http.routes.*
+
+class HttpApi[F[_]: Monad] private {
+  private val healthRoutes = HealthRoutes[F]
+  private val jobRoutes    = JobRoutes[F]
+
+  val endpoints = Router(
+    "/api" -> (healthRoutes.routes <+> jobRoutes.routes)
+  )
+}
+
+object HttpApi {
+  def apply[F[_]: Monad]: HttpApi[F] = new HttpApi[F]
+}
--- src/main/scala/com/rockthejvm/jobsboard/http/routes/JobRoutes.scala
+++ src/main/scala/com/rockthejvm/jobsboard/http/routes/JobRoutes.scala
@@ -5,45 +5,38 @@ import org.http4s.dsl.*
 import org.http4s.dsl.impl.*
 import org.http4s.server.*
 import cats.Monad
+import com.comcast.ip4s.Literals.idn
+import cats.implicits.*
 
 class JobRoutes[F[_]: Monad] extends Http4sDsl[F] {
   // POST /jobs?offset=x&limit=y { filters }  // TODO: add query params and filtesr
-  private val allJobsRoute: HttpRoutes[F] = {
-    HttpRoutes.of[F] { case GET -> Root / "jobs" =>
-      Ok("All jobs!")
-    }
+  private val allJobsRoute: HttpRoutes[F] = HttpRoutes.of[F] { case POST -> Root =>
+    Ok("TODO")
   }
 
   // GET /jobs/{uuid}
-  private val jobRoute: HttpRoutes[F] = {
-    HttpRoutes.of[F] { case GET -> Root / "jobs" / UUIDVar(uuid) =>
-      Ok(s"Job $uuid")
-    }
+  private val jobRoute: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root / UUIDVar(id) =>
+    Ok(s"TODO find job for $id")
   }
 
-  // POST /jobs { job }
-  private val createJobRoute: HttpRoutes[F] = {
-    HttpRoutes.of[F] { case req @ POST -> Root / "jobs" =>
-      Ok("Job created!")
-    }
+  // POST /jobs / create { job }
+  private val createJobRoute: HttpRoutes[F] = HttpRoutes.of[F] { case POST -> Root / "create" =>
+    Ok("TODO")
   }
 
   // PUT /jobs/{uuid} { jobInfo }
-  private val updateJobRoute: HttpRoutes[F] = {
-    HttpRoutes.of[F] { case req @ PUT -> Root / "jobs" / UUIDVar(uuid) =>
-      Ok(s"Job $uuid updated!")
-    }
+  private val updateJobRoute: HttpRoutes[F] = HttpRoutes.of[F] { case PUT -> Root / UUIDVar(id) =>
+    Ok(s"TODO update job at $id")
   }
 
   // DELETE /jobs/{uuid}
-  private val deleteJobRoute: HttpRoutes[F] = {
-    HttpRoutes.of[F] { case DELETE -> Root / "jobs" / UUIDVar(uuid) =>
-      Ok(s"Job $uuid deleted!")
-    }
+  private val deleteJobRoute: HttpRoutes[F] = HttpRoutes.of[F] {
+    case DELETE -> Root / UUIDVar(id) =>
+      Ok(s"TODO delete job at $id")
   }
 
   val routes = Router(
-    "/jobs" -> ??? // complete this
+    "/jobs" -> (allJobsRoute <+> jobRoute <+> createJobRoute <+> updateJobRoute <+> deleteJobRoute)
   )
 }
 
